3da39844da26180ba0f2d694a69dd01c81bf5cc0
MounikaCodes <mounikalogins@gmail.com>
2024-05-30 12:34:01 +0530

    login with api is done

--- one-skill/src/App.js
+++ one-skill/src/App.js
@@ -1,11 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Registration from "./components/Registration";
+import Registration from "./screens/Registration";
+import Login from "./screens/Login";
+import Dashboard from "./screens/Dashboard";
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Registration />} />
-        <Route path="/register" element={<Registration />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/Dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
   );
---
+++ one-skill/src/screens/Dashboard.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Dashboard = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="bg-white p-8 rounded shadow-md w-full max-w-md">
+        <h1 className="text-2xl font-bold mb-6 text-center">Dashboard</h1>
+        <p className="text-center">Welcome to the Dashboard!</p>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
